b3dbd039180e36e2d3b87b44bf57a5d8b8d9e938
Денис Баранчиков <denysbaranchykov@MacBook-Air-Denis.local>
2022-07-16 19:51:20 +0200

    initial

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+
+const Profile = () => {
+    return   <div className = 'content'>
+    <div>
+    <img src ='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
