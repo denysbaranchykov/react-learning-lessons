5be5f9c2035d7329551425570a5c1354ee4c3b48
denysbaranchykov <denysbaranchykov@gmail.com>
2022-07-21 13:29:27 +0200

    active in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,33 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div classname={s.dialog + '' + s.active}>
+                    Dimych
+                </div>
+                <div classname={s.dialog}>
+                    Andrey
+                </div>
+                <div classname={s.dialog}>
+                    Sveta
+                </div>
+                <div classname={s.dialog}>
+                    Sasha
+                </div>
+                <div classname={s.dialog}>
+                    Viktor
+                </div>
+                <div classname={s.dialog}>
+                    Valera
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra? </div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
