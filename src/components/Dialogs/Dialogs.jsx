28f4af761852bc51cf4fd263a9bc03fc856ebec7
denysbaranchykov <denysbaranchykov@gmail.com>
2022-08-01 23:15:51 +0200

    each child key prop

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,10 +6,11 @@ import Message from './Message/Message';
 
 
 const Dialogs = (props) => {
-    const dialogsElements = props.state.dialogs.map(d =>
-        <DialogItem name={d.name} id={d.id} />);
-    const messagesElements = props.state.messages.map(m =>
-        <Message message={m.message} />);
+    const dialogsElements = props.state.dialogs.map((d,index) =>
+        <DialogItem name={d.name} id={d.id}
+        key={index+d.id}/>);
+    const messagesElements = props.state.messages.map((m,index) =>
+        <Message message={m.message} key={index+m.message} />);
 
 
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,7 +4,7 @@ const ProfileInfo = () => {
   return (
     <div>
       <div>
-        <img src='https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=1100&q=60' width="200" />
+        <img src='https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=1100&q=60'  width="200" />
       </div>
       <div className={s.descriptionBlock}>
         ava + description
