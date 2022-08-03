3654bb28b096ac32613f2cbb1153c419169ac8df
denysbaranchykov <denysbaranchykov@gmail.com>
2022-08-03 18:14:02 +0200

    redux initialization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/state';
+import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -23,7 +23,10 @@ const rerenderEntireTree = (state) => {
 }
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree);
+store.subscribe(()=> {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 
 
 // If you want to start measuring performance in your app, pass a function
