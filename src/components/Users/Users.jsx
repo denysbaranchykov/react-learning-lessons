5e205061ff67f8761c0ddbefea029c95a0f0449f
denysbaranchykov <denysbaranchykov@gmail.com>
2022-08-09 23:19:11 +0200

    setUsers(warnings)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,37 @@ import React from "react";
 import styles from './users.module.css'
 
 const Users = (props) => {
+    if(props.users.length === 0) {
+
+        props.setUsers(
+            [
+                {
+                    id: 1,
+                    photoUrl: 'https://thumbs.dreamstime.com/z/happy-kids-boy-play-swim-guard-lifeguard-sea-funny-child-boy-lifebuoy-show-thumbs-up-sign-happy-kids-boy-play-196171410.jpg',
+                    followed: false,
+                    fullName: 'Denys',
+                    status: 'I am a boss',
+                    location: {city: 'Wroclaw', country: 'Poland'}
+                },
+                {
+                    id: 2,
+                    photoUrl: 'https://static3.depositphotos.com/1003827/178/i/600/depositphotos_1788160-stock-photo-mad-programmer.jpg',
+                    followed: true,
+                    fullName: 'Yevhenii',
+                    status: 'Sensei',
+                    location: {city: 'Bryton', country: 'England'}
+                },
+                {
+                    id: 3,
+                    photoUrl: 'https://www.icevirtuallibrary.com/pb-assets/journal%20news/2020/Green-construction-1637592067220.jpg',
+                    followed: false,
+                    fullName: 'Sasha',
+                    status: 'Engineer',
+                    location: {city: 'Kharkiv', country: 'Ukraine'}
+                },
+            ]
+        )
+    }
     return <div>
         {props.users.map(u => <div key={u.id}>
             <span>
