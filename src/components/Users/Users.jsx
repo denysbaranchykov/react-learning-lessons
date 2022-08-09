a53385b4abb9986b63620a6bf261812bd4809a2c
denysbaranchykov <denysbaranchykov@gmail.com>
2022-08-10 00:58:39 +0200

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,6 +4,8 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
+
+    const getUsers = () => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             props.setUsers(response.data.items
@@ -35,8 +37,9 @@ const Users = (props) => {
                 // ]
             );
         });
-    }
+    }}
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
@@ -44,12 +47,8 @@ const Users = (props) => {
                 </div>
                 <div>
                     {u.followed
-                        ? <button onClick={() => {
-                            props.unfollow(u.id)
-                        }}>Unfollow</button>
-                        : <button onClick={() => {
-                            props.follow(u.id)
-                        }}>Follow</button>}
+                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                        : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
                 </div>
             </span>
             <span>
