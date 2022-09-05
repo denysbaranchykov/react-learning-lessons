ec1859010d0b30cac68740d500decc3c2fa07db6
denysbaranchykov <denysbaranchykov@gmail.com>
2022-09-05 18:46:14 +0200

    81 -  Selectors part 1,mistakes with follow and unfollow users

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -12,7 +12,7 @@ let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
-    currentPage: 1,
+    page: 1,
     isFetching: true,
     followingInProgress: []
 };
@@ -76,10 +76,11 @@ export const toggleFollowingProgress = (isFetching, userId) => ({
     userId
 });
 
-export const getUsers = (currentPage, pageSize) => {
+export const getUsers = (page, pageSize) => {
     return (dispatch) => {
         dispatch(toggleIsFetching(true));
-        usersAPI.getUsers(currentPage, pageSize).then(data => {
+        dispatch(setCurrentPage(page));
+        usersAPI.getUsers(page, pageSize).then(data => {
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
             dispatch(setTotalCountUsers(data.totalCount));
